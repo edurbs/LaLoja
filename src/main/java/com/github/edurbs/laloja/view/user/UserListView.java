f7b4369ef8be929c26de4c5096b9d79ef2316e2c
Eduardo Soares <edurbs@gmail.com>
2025-01-08 12:35:05 -0300

    first commit

---
+++ src/main/java/com/github/edurbs/laloja/view/user/UserListView.java
@@ -0,0 +1,14 @@
+package com.github.edurbs.laloja.view.user;
+
+import com.github.edurbs.laloja.entity.User;
+import com.github.edurbs.laloja.view.main.MainView;
+import com.vaadin.flow.router.Route;
+import io.jmix.flowui.view.*;
+
+@Route(value = "users", layout = MainView.class)
+@ViewController(id = "User.list")
+@ViewDescriptor(path = "user-list-view.xml")
+@LookupComponent("usersDataGrid")
+@DialogMode(width = "64em")
+public class UserListView extends StandardListView<User> {
+}
