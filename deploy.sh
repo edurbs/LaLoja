e999025dd059163da29d7f3e4663cedcf7698b6b
Eduardo Soares <edurbs@gmail.com>
2025-01-09 14:39:16 -0300

    add docker build

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+rm build/libs/*.jar
+
+./gradlew bootjar
+
+docker build -t laloja:latest .
+
+docker tag laloja:latest edurbs/laloja:latest
+
+docker push edurbs/laloja:latest
+
+ssh root@192.168.21.201 '/root/deployLaloja.sh'
+
+
+
