735b8aa9437c6835ab622fd97557d9852cb09658
Eduardo Soares <edurbs@gmail.com>
2025-01-10 01:48:43 -0300

    set button receive

--- src/main/java/com/github/edurbs/laloja/view/income/IncomeListView.java
+++ src/main/java/com/github/edurbs/laloja/view/income/IncomeListView.java
@@ -1,12 +1,21 @@
 package com.github.edurbs.laloja.view.income;
 
 import com.github.edurbs.laloja.entity.Income;
+import com.github.edurbs.laloja.entity.IncomePayment;
 import com.github.edurbs.laloja.entity.Person;
+import com.github.edurbs.laloja.view.incomepayment.IncomePaymentDetailView;
 import com.github.edurbs.laloja.view.main.MainView;
+import com.vaadin.flow.data.selection.SelectionEvent;
+import com.vaadin.flow.router.QueryParameters;
 import com.vaadin.flow.router.Route;
 import io.jmix.core.Metadata;
+import io.jmix.flowui.ViewNavigators;
+import io.jmix.flowui.component.grid.DataGrid;
 import io.jmix.flowui.component.propertyfilter.PropertyFilter;
+import io.jmix.flowui.kit.action.ActionPerformedEvent;
+import io.jmix.flowui.kit.component.button.JmixButton;
 import io.jmix.flowui.view.*;
+import io.jmix.flowui.view.navigation.ViewNavigator;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @Route(value = "incomes", layout = MainView.class)
@@ -19,6 +28,12 @@ public class IncomeListView extends StandardListView<Income> {
     private PropertyFilter<Person> personPropertyFilter;
     @Autowired
     private Metadata metadata;
+    @ViewComponent
+    private DataGrid<Income> incomesDataGrid;
+    @Autowired
+    private ViewNavigators viewNavigators;
+    @ViewComponent
+    private JmixButton payButton;
 
     @Install(to = "incomesDataGrid.create", subject = "newEntitySupplier")
     private Income incomesDataGridCreateNewEntitySupplier() {
@@ -26,5 +41,23 @@ public class IncomeListView extends StandardListView<Income> {
         Income income = metadata.create(Income.class);
         income.setPerson(person);
         return income;
+    }
+
+    @Subscribe("incomesDataGrid.addPayment")
+    public void onIncomesDataGridAddPayment(final ActionPerformedEvent event) {
+        Income income = incomesDataGrid.getSingleSelectedItem();
+        if (income == null ) return ;
+        viewNavigators.detailView(this, IncomePayment.class)
+                .newEntity()
+                .withQueryParameters(QueryParameters.of("income", income.getId().toString()))
+                .navigate();
+
+    }
+
+    @Subscribe("incomesDataGrid")
+    public void onIncomesDataGridSelection(final SelectionEvent<DataGrid<Income>, Income> event) {
+        event.getFirstSelectedItem().ifPresentOrElse(
+                income -> payButton.setEnabled(true),
+                () -> payButton.setEnabled(false));
     }
 }
--- src/main/java/com/github/edurbs/laloja/view/incomepayment/IncomePaymentDetailView.java
+++ src/main/java/com/github/edurbs/laloja/view/incomepayment/IncomePaymentDetailView.java
@@ -1,17 +1,24 @@
 package com.github.edurbs.laloja.view.incomepayment;
 
+import com.github.edurbs.laloja.entity.Income;
 import com.github.edurbs.laloja.entity.IncomePayment;
 import com.github.edurbs.laloja.view.main.MainView;
 import com.vaadin.componentfactory.addons.inputmask.InputMask;
 import com.vaadin.flow.router.Route;
+import io.jmix.core.DataManager;
+import io.jmix.core.Metadata;
 import io.jmix.flowui.component.datepicker.TypedDatePicker;
 import io.jmix.flowui.component.textfield.TypedTextField;
+import io.jmix.flowui.component.valuepicker.EntityPicker;
 import io.jmix.flowui.view.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.vaadin.textfieldformatter.NumeralFieldFormatter;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.Date;
+import java.util.List;
+import java.util.UUID;
 
 @Route(value = "incomePayments/:id", layout = MainView.class)
 @ViewController(id = "IncomePayment.detail")
@@ -23,6 +30,14 @@ public class IncomePaymentDetailView extends StandardDetailView<IncomePayment> {
 
     @ViewComponent
     private TypedTextField<BigDecimal> totalField;
+    @Autowired
+    private Metadata metadata;
+    @Autowired
+    private DataManager dataManager;
+
+    private Income income;
+    @ViewComponent
+    private EntityPicker<Income> incomeField;
 
     @Subscribe
     public void onInit(final InitEvent event) {
@@ -37,7 +52,25 @@ public class IncomePaymentDetailView extends StandardDetailView<IncomePayment> {
     }
 
     @Subscribe
+    public void onBeforeShow(final BeforeShowEvent event) {
+        incomeField.setValue(income);
+    }
+
+    @Subscribe
     public void onInitEntity(final InitEntityEvent<IncomePayment> event) {
         event.getEntity().setPaymentDate(LocalDate.now());
     }
+
+    @Subscribe
+    public void onQueryParametersChange(final QueryParametersChangeEvent event) {
+        List<String> messageParams = event.getQueryParameters().getParameters().get("income");
+        if (messageParams != null && !messageParams.isEmpty()) {
+            String uuid = messageParams.getFirst();
+            this.income = dataManager.load(Income.class).id(UUID.fromString(uuid)).one();
+        }
+    }
+
+
+
+    
 }
